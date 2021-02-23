7c9a3f4df2ea9087540cd48af83545122c559adf
Hannah Tabunan <hannah.tabunan@gmail.com>
2021-02-23 14:50:29 +0800

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '374070c8e7559ead14fba7cb7b8c0993db7780e9',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '374070c8e7559ead14fba7cb7b8c0993db7780e9',
+    ),
+  ),
+);
